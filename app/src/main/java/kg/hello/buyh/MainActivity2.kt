cb0158f19b41e733f63ac36442b8a4617c05c77c
adelinaotonbaeva <otonbaeva_a@auca.kg>
2021-11-23 15:59:14 +0600

    adelina cam

--- app/src/main/java/kg/hello/buyh/MainActivity2.kt
+++ app/src/main/java/kg/hello/buyh/MainActivity2.kt
@@ -4,15 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kg.hello.buyh.R.layout.mainactivity2
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.mainactivity2)
         val button = findViewById<Button>(R.id.button)
 
         button.setOnClickListener {
-            startActivity(Intent(this, MainActivity2::class.java))
+            startActivity(Intent(this, Reklama::class.java))
         }
     }
 }
