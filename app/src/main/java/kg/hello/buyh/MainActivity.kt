27509697184ed75992989fba562902be672e3737
adelinaotonbaeva <otonbaeva_a@auca.kg>
2021-12-12 21:35:35 +0600

    File adelina.xml

--- app/src/main/java/kg/hello/buyh/MainActivity.kt
+++ app/src/main/java/kg/hello/buyh/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.startbutton)
+        val button = findViewById<Button>(R.id.start_button)
 
         button.setOnClickListener {
             startActivity(Intent(this, Login::class.java))
